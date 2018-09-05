f6870fb78df615f14476cf7e77f0251c291f9bb9
wkiwi <15619986615@163.com>
2018-09-05 19:51:42 +0800

    home

---
+++ src/components/home/Home.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <home-header></home-header>
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+import HomeHeader from './components/Header'
+export default {
+  name: 'Home',
+  components: {
+      HomeHeader
+  },
+  methods: {
+    getInfo: function () {
+        axios.get('/api/data.json')
+        .then(this.handleGitInfoSucc)  
+    },
+    handleGitInfoSucc: function (res) {
+        console.log(res)
+    }
+  },
+  mounted () {
+        this.getInfo()
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/home/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="header">
+        头部
+    </div>
+</template>
+
+<script type="text/ecmascript-6">
+export default {
+    name: 'HomeHeader',
+    data () {
+        return {
+
+        }
+    },
+    components: {
+
+    }
+}
+</script>
+
+<style  lang="stylus" scoped>
+
+</style>
